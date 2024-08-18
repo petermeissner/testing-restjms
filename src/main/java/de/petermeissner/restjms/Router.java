e6f4009805880ff7c9c133df4fad96e95867808c
Peter Meißner <retep.meissner@gmail.com>
2024-08-18 09:31:30 +0200

    2nd endpoint

--- src/main/java/de/petermeissner/restjms/Router.java
+++ src/main/java/de/petermeissner/restjms/Router.java
@@ -4,11 +4,18 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
-@Path("/hello")
+@Path("/")
 public class Router {
     @GET
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    public String apiRoot() {
+        return "API-Root";
+    }
+
+    @GET
+    @Path("/dings")
+    @Produces("text/plain")
+    public String dings() {
+        return "dings";
     }
 }
